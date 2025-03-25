273c2c5f5d8ce9d7fa58161935b780d937770882
prepodobnuy <prepodobnuy@inbox.ru>
2025-03-25 23:37:30 +0300

    added endpoints for "title"

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -3,12 +3,14 @@ from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse
 
 from .auth import router as auth_router
 from .user import router as user_router
+from .title import router as title_router
 from etc.static import tags, genres
 
 app = FastAPI()
 
 app.include_router(auth_router, prefix='/auth', tags=['auth'])
 app.include_router(user_router, prefix='/user', tags=['user'])
+app.include_router(title_router, prefix='/title', tags=['title'])
 
 @app.get('/tags')
 async def get_tags():
---
+++ server/api/title.py
@@ -0,0 +1,147 @@
+from io import BytesIO
+
+from etc.static import DEFAULT_COVER
+from etc.role import role_is_moder, role_is_muted
+from .needed import APIRouter, FileResponse, HTTPException, UploadFile, Depends, File
+from .needed import get_current_user, get_current_user_unsafe, session
+from schemas.title import TitleMetaPostScheme
+from schemas.title import TitleMetaGetScheme
+from database.tables import TitleMeta, Title, User
+
+router = APIRouter()
+
+
+@router.get('/{id}')
+async def get_title_meta(id: int, user: User = Depends(get_current_user_unsafe)) -> TitleMetaGetScheme:
+    title_meta = session.query(TitleMeta).filter(TitleMeta.id == id).first()
+
+    if title_meta is None: 
+        raise HTTPException(404)
+
+    if user is None:
+        if title_meta.public:
+            return title_meta.get(session=session, uuid=None)
+
+        raise HTTPException(403)
+    
+    if title_meta.public:
+        return title_meta.get(session=session, uuid=user.uuid)
+    
+    if role_is_moder(user.role):
+        return title_meta.get(session=session, uuid=user.uuid)
+
+    raise HTTPException(403)
+
+
+@router.post('')
+async def post_title(scheme: TitleMetaPostScheme, user: User = Depends(get_current_user)) -> int:
+    if role_is_muted(user.role):
+        raise HTTPException(403)
+    
+    meta = TitleMeta.new(session=session, scheme=scheme, uuid=user.uuid)
+
+    try:
+        session.add(meta)
+        session.commit()
+        return meta.id
+    except Exception as e:
+        print(e)
+        session.rollback()
+        raise HTTPException(500)
+    
+
+@router.post('/{title_meta_id}/cover')
+async def post_title_cover(title_meta_id: int, file: UploadFile = File(...), user: User = Depends(get_current_user)):
+    if role_is_muted(user.role):
+        raise HTTPException(403)
+    
+    title_meta = session.query(TitleMeta).filter(TitleMeta.id == title_meta_id).first()
+
+    if title_meta is None:
+        raise HTTPException(404)
+    
+    if  title_meta.user_uuid != user.uuid and not role_is_moder(user.role):
+        raise HTTPException(403)
+    
+    if not file.content_type.startswith("image/"):
+        raise HTTPException(400, 'file is not an image')
+
+    contents = await file.read()
+    title_meta.set_cover(BytesIO(contents))
+
+
+@router.put('/{title_id}')
+async def update_title_meta(title_id: int, scheme: TitleMetaPostScheme, user: User = Depends(get_current_user)) -> int:
+    if role_is_muted(user.role):
+        raise HTTPException(403)
+
+    title = session.query(Title).filter(Title.id == title_id).first()
+
+    if title is None:
+        raise HTTPException(404)
+    
+    meta = TitleMeta.new(session=session, scheme=scheme, uuid=user.uuid)
+
+    if meta is None:
+        raise HTTPException(423, 'too many pending data')
+
+    try:
+        session.add(meta)
+        session.commit()
+        return meta.id
+    except Exception as e:
+        print(e)
+        session.rollback()
+        raise HTTPException(500)
+    
+
+@router.post('approve/{title_meta_id}')
+async def approve_title_meta(title_meta_id: int, user: User = Depends(get_current_user)):
+    if not role_is_moder(user.role):
+        raise HTTPException(403)
+
+    meta = session.query(TitleMeta).filter(TitleMeta.id == title_meta_id).first()
+
+    if meta is None:
+        raise HTTPException(404)
+    
+    if meta.public is True:
+        raise HTTPException(400)
+    
+    current_approved_meta = session.query(TitleMeta).filter(TitleMeta.public is True).first()
+
+    if current_approved_meta is not None:
+        try:
+            session.delete(current_approved_meta)
+            session.commit()
+        except Exception as e:
+            print(e)
+            session.rollback()
+            raise HTTPException(500)
+        
+    meta.public = True
+    try:
+        session.commit()
+    except Exception as e:
+        print(e)
+        session.rollback()
+        raise HTTPException(500)
+    
+
+@router.post('disapprove/{title_id}')
+async def disapprove_title_meta(title_id: int, user: User = Depends(get_current_user)):
+    if not role_is_moder(user.role):
+        raise HTTPException(403)
+    
+    meta = session.query(TitleMeta).filter(TitleMeta.title_id == title_id).filter(TitleMeta.public is True).first()
+
+    if meta is None:
+        raise HTTPException(404)
+    
+    try:
+        session.delete(meta)
+        session.commit()
+    except Exception as e:
+        print(e)
+        session.rollback()
+        raise HTTPException(500)
